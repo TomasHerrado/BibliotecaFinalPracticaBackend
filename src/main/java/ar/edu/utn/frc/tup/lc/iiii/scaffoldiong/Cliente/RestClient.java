5219def3434899ac98b07ab43e933c5b6de060ac
114112_Herrado_Tomas <114112@tecnicatura.frc.utn.edu.ar>
2025-11-11 15:17:02 -0300

    commit

--- src/main/java/ar/edu/utn/frc/tup/lc/iiii/scaffoldiong/Cliente/RestClient.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iiii/scaffoldiong/Cliente/RestClient.java
@@ -2,6 +2,7 @@ package ar.edu.utn.frc.tup.lc.iiii.scaffoldiong.Cliente;
 
 import ar.edu.utn.frc.tup.lc.iiii.scaffoldiong.entity.Persona;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -16,22 +17,11 @@ import java.util.List;
 public class RestClient {
     private final RestTemplate restTemplate;
 
-//    public List<Persona> getPersona() {
-//        String url = "https://run.mocky.io/v3/91f86e81-98d4-44d9-9ca9-b0b5ac72c78d";
-//        try {
-//            ResponseEntity<List<Persona>> response = restTemplate.exchange(
-//                    url,
-//                    HttpMethod.GET,
-//                    null,
-//                    new ParameterizedTypeReference<List<Persona>>() {
-//                    });
-//            return response.getBody();
-//        } catch (RestClientException ex) {
-//            throw new IllegalStateException("Error al comunicarse con la API externa de personas", ex);
-//        }
-//    }
+    @Value("${personas.service.url}")
+    private String personasServiceUrl;
+
     public List<Persona> getPersona() {
-        String url = "http://localhost:8081/api/personas";
+        String url = personasServiceUrl + "/api/personas";
         try {
             ResponseEntity<List<Persona>> response = restTemplate.exchange(
                     url,
@@ -46,7 +36,7 @@ public class RestClient {
     }
 
     public Persona getPersonaAleatoria() {
-        String url = "http://localhost:8081/api/personas/aleatorio";
+        String url = personasServiceUrl + "/api/personas/aleatorio";
         try {
             return restTemplate.getForObject(url, Persona.class);
         } catch (RestClientException ex) {
